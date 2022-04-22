f0fca798f80f0e240aeef369652d8ea476c27808
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-04-21 22:27:02 -0400

    removing k8s version

--- aks.tf
+++ aks.tf
@@ -3,7 +3,7 @@ resource "azurerm_kubernetes_cluster" "this" {
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
   dns_prefix          = "aks1"
-  kubernetes_version  = "1.18.14"
+#   kubernetes_version  = "1.18.14"
 
   default_node_pool {
     name                = "default"
