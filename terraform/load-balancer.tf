26f7d6159740b1562f72cbe16aba6f2827f90cf3
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-05-03 22:39:46 -0400

    removing subnet from lb (pip in use)

--- terraform/load-balancer.tf
+++ terraform/load-balancer.tf
@@ -14,8 +14,6 @@ resource "azurerm_lb" "this" {
     frontend_ip_configuration {
         name                 = "PublicIPAddress"
         public_ip_address_id = azurerm_public_ip.this.id
-        subnet_id            = var.vm_subnet_id
-
     }
 }
 
